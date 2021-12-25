8b3821f0af2cb5a3131b68142f10c76687e201bc
wolfgangdepaula <depaulawolfgang@gmail.com>
2021-12-25 13:19:39 -0400

    fisrt commit wolf

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:irrigador/screens/shop_screen.dart';
 
 void main() => runApp(MyApp());
 
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
